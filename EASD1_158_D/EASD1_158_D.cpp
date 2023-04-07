40098c4a81f0b78bfefe9c48cfcc24c519f61f30
iqbaldf7 <iqbalfathonah10@gmail.com>
2023-04-07 10:48:13 +0700

    menambahkan 2nim

--- EASD1_158_D/EASD1_158_D.cpp
+++ EASD1_158_D/EASD1_158_D.cpp
@@ -1,26 +1,55 @@
-#include <iostream>
-
+/**
+1. Karena algoritma dibutuhkan untuk menyelesaikan masalah secara sistematis
+2. Statis & Dinamis
+3. membuat codingan yang tidak diperlukan
+4. Menurut saya Insertion sort karena membandingkan dan memindahkan elemen ke posisi yang tepat didalam susunan data yang sudah diurutkan
+5. Quadratic : Bubble Sort, Selection Sort, dan Insertion Sort
+	Loglinear : Merge Sort dan Quick Sort
+6. Selection sort:
+**/
+#include<iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n) {
-    for (int j = 0; j < n - 1; j++) {
-        int min_index = j;
-        for (int i = j + 1; i < n; i++) {
-            if (arr[i] < arr[min_index]) {
-                min_index = i;
-            }
-        }
-        swap(arr[j], arr[min_index]);
-    }
+
+const int i = 58 + 20 - 2 * 15 + 10 + 20;
+int iqbal[i];
+int n;
+
+void input() {
+	cout << "Masukkan " << n << " angka : ";
+	for (int i = 0; i < n; i++) {
+		cin >> iqbal[i];
+	}
+}
+
+void selectionsort() {
+	for (int ID = 0; ID < n; ID++) {
+		int min_index = ID;
+		for (int i = ID + 1;i < n;i++) {
+			if (iqbal[i] < iqbal [min_index]) {
+				min_index = i;
+			}
+		}
+		if (min_index != ID) {
+			swap(iqbal[ID], iqbal[min_index]);
+		}
+	}
+}
+
+void display() {
+
+	cout << "Data urut : ";
+	for (int i = 0;i < n;i++) {
+		cout << iqbal[i] << " ";
+	}
+	cout << endl;
 }
 
 int main() {
-    int arr[] = { 64, 25, 12, 22, 11 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    selectionSort(arr, n);
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    return 0;
+	cout << "Masukkan Jumlah data (Maksimum " << i << "): ";
+	cin >> n;
+
+	input();
+	selectionsort();
+	display();
 }
